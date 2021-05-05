22d01e1cf39d8d7cd7f90163d55fce6bc7aba8d0
Tushar Bhatt <tbhatt321@gmail.com>
2021-05-05 18:20:18 +0530

    Updates

--- precache-manifest.4a2f96f2c5206b54afbb892171afe245.js
+++ precache-manifest.4a2f96f2c5206b54afbb892171afe245.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "73dd8802d980bd2d684ddcf6d2e4cb49",
+    "revision": "866d7a984cc9edcc73c181d8823ca754",
     "url": "/automata-simulator/index.html"
   },
   {
-    "revision": "e46aa324fe77565f1f7a",
-    "url": "/automata-simulator/static/css/main.3da08041.chunk.css"
+    "revision": "397c71b47742ab86f46b",
+    "url": "/automata-simulator/static/css/main.1e0292d4.chunk.css"
   },
   {
     "revision": "2f2ef42dafc98f8c51fc",
     "url": "/automata-simulator/static/js/2.3dede7a7.chunk.js"
   },
   {
-    "revision": "e46aa324fe77565f1f7a",
-    "url": "/automata-simulator/static/js/main.2796aa91.chunk.js"
+    "revision": "397c71b47742ab86f46b",
+    "url": "/automata-simulator/static/js/main.6b1ee2c8.chunk.js"
   },
   {
     "revision": "1510c7607038ce5d0e92",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/automata-simulator/precache-manifest.5311f0ced421c5a70de8771da1d64080.js"
+  "/automata-simulator/precache-manifest.4a2f96f2c5206b54afbb892171afe245.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/automata-simulator/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
